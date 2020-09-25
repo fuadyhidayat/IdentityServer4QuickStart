ef71334371fa387b1e3fa632fdad0fe8c5310a3f
Fuady Rosma Hidayat <fuady@live.com>
2020-09-26 01:16:43 +0700

    Menggunakan TestUsers.Users

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("PCMSAPIforConsole", "PCMS API for Console")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "PCMSConsole",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("PCMSConsoleSecret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "PCMSAPIforConsole" }
+                }
+            };
+    }
+}
